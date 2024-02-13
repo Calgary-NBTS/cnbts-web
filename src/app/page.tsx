6ef76cb0c267ee64000556b3f4b323a39c5a4e80
JenniferFix <codingjennifer@gmail.com>
2024-02-13 14:11:40 -0700

    Change page title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
 import Image from "next/image";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Calgary Non-Binary and Transgender Society',
+  description: 'Calgary Non-Binary and Transgender Societies official homepage'
+}
 
 export default function Home() {
   return (
