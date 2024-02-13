182ae30d2f632fc10102921da551c0f01ba91650
JenniferFix <codingjennifer@gmail.com>
2024-02-13 14:50:02 -0700

    update icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,4 @@
 import Image from "next/image";
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: 'Calgary Non-Binary and Transgender Society',
-  description: 'Calgary Non-Binary and Transgender Societies official homepage'
-}
 
 export default function Home() {
   return (
