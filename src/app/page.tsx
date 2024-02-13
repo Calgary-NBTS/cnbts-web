12c1c59ebdc8f8ac639d50ccd58922a389c879ee
JenniferFix <codingjennifer@gmail.com>
2024-02-13 14:08:46 -0700

    update temporary page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,13 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <h1 className="text-2xl">Calgary Non-Binary and Transgender Society</h1>
-        <p>Coming Soon</p>
+        <Image
+          src="/BlackLogo.jpeg"
+          alt="Calgary Non-Binary and Transgender Society Logo - Trans Flag and Transgender Symbol Wrapped with Dragon"
+          width={1000} 
+          height={1000}
+          />
+          <p className="flex items-center justify-center">Coming Soon</p>
         </div>
     </main>
   );
