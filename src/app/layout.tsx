69f544aa9979dbd0dd06576b00e603a6adb4bb2b
JenniferFix <codingjennifer@gmail.com>
2024-02-18 18:11:10 -0700

    cleanup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
-import type { Metadata } from "next";
-import "./globals.css";
-// import Header from "@/components/Header";
-import Header from "../components/Header";
+import type { Metadata } from 'next';
+import './globals.css';
+import Header from 'src/components/Header';
+import Footer from 'src/components/Footer';
 
 export const metadata: Metadata = {
   title: "Calgary Non-Binary and Transgender Society",
