5965c97c30ddfa21694a04f10fbd8655fe516426
JenniferFix <codingjennifer@gmail.com>
2024-02-18 18:12:28 -0700

    Add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,18 @@
+import { FaFacebook, FaDiscord, FaSquareInstagram } from "react-icons/fa6";
+
+const Footer = () => {
+    return (
+        <div className="p-4 bg-sky-400">
+            <div className="m-2">
+                <p>Come join us at any or all of the below platorms.</p>
+            </div>
+            <div className="m-2 flex gap-4 text-4xl">
+                <div><a href="" rel="noopener noreferrer"><FaFacebook /></a></div>
+                <div><a href="" rel="noopener noreferrer"><FaDiscord /></a></div>
+                <div><a href="" rel="noopener noreferrer"><FaSquareInstagram /></a></div>
+            </div>
+        </div>
+    );
+}
+    
+export default Footer;
