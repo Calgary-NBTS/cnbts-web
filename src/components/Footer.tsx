20a3deba3838fc5e7e19621c65c5f39a31dbd2da
JenniferFix <codingjennifer@gmail.com>
2024-02-18 23:48:03 -0700

    Add socials

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,9 +7,9 @@ const Footer = () => {
                 <p>Come join us at any or all of the below platorms.</p>
             </div>
             <div className="m-2 flex gap-4 text-4xl">
-                <div><a href="" rel="noopener noreferrer"><FaFacebook /></a></div>
-                <div><a href="" rel="noopener noreferrer"><FaDiscord /></a></div>
-                <div><a href="" rel="noopener noreferrer"><FaSquareInstagram /></a></div>
+                <div><a href="https://www.facebook.com/groups/1494690848018790" rel="noopener noreferrer"><FaFacebook /></a></div>
+                <div><a href="https://discord.gg/t7a9xFmfcA" rel="noopener noreferrer"><FaDiscord /></a></div>
+                <div><a href="https://www.instagram.com/calgary_enby_trans_society?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" rel="noopener noreferrer"><FaSquareInstagram /></a></div>
             </div>
         </div>
     );
