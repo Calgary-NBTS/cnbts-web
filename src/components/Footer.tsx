8da78a07d9b0e77e8056dda2ab30de403e38a8da
JenniferFix <codingjennifer@gmail.com>
2024-02-23 22:22:04 -0700

    Added aria labls to social icon links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,12 +7,12 @@ const Footer = () => {
                 <p>Come join us at any or all of the below platorms.</p>
             </div>
             <div className="m-2 flex gap-4 text-4xl">
-                <div><a href="https://www.facebook.com/groups/1494690848018790" rel="noopener noreferrer"><FaFacebook /></a></div>
-                <div><a href="https://discord.gg/t7a9xFmfcA" rel="noopener noreferrer"><FaDiscord /></a></div>
-                <div><a href="https://www.instagram.com/calgary_enby_trans_society?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" rel="noopener noreferrer"><FaSquareInstagram /></a></div>
+                <div><a href="https://www.facebook.com/groups/1494690848018790" rel="noopener noreferrer" aria-label="Facebook"><FaFacebook /></a></div>
+                <div><a href="https://discord.gg/t7a9xFmfcA" rel="noopener noreferrer" aria-label="Discord"><FaDiscord /></a></div>
+                <div><a href="https://www.instagram.com/calgary_enby_trans_society?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" rel="noopener noreferrer" aria-label="Instagram"><FaSquareInstagram /></a></div>
             </div>
         </footer>
     );
 }
-    
+   
 export default Footer;
