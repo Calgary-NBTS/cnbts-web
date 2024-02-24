132aee5bd434b22d1faaebba0051808f30c5bdb1
JenniferFix <codingjennifer@gmail.com>
2024-02-24 04:46:43 -0700

    Work on events calendar

--- src/components/CalendarPage.tsx
+++ src/components/CalendarPage.tsx
@@ -2,10 +2,17 @@ import { getEvents } from '@/sanity/sanity-utils';
 import Image from 'next/image';
 import EventsCalendar from './EventsCalendar';
 
-const  CalendarPage = ({year,month,day}:{year?: Number; month?: Number; day?: Number;}) => {
+const  CalendarPage = async ({year,month,day}:{year?: Number; month?: Number; day?: Number;}) => {
+    const events = await getEvents();
+
+    const eventDays = events.map(event=>new Date(event.time).getTime());
+    const first = new Date(Math.min(...eventDays));
+    const last = new Date(Math.max(...eventDays));
+
+    console.log(first,last);
 
     return <div className="my-6 mx-auto w-full">
-        <EventsCalendar />
+        <EventsCalendar events={events} />
 {/* 
         <div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             {events.map((event) => (
--- src/components/EventsCalendar.tsx
+++ src/components/EventsCalendar.tsx
@@ -5,19 +5,26 @@ import { Event } from '@/sanity/types/queryTypes';
 
 import { FaDiscord } from "react-icons/fa6";
 
-import Calendar, { TileArgs } from 'react-calendar';
+import Calendar, { OnArgs, TileArgs } from 'react-calendar';
 import './css/Calendar.css';
+import { act } from 'react-dom/test-utils';
 
 type ValuePiece = Date | null;
 type Value = ValuePiece | [ValuePiece, ValuePiece];
 
-type EventsCalType = {
-    events?: Event[];
-}
-
-export default function EventsCalendar({events}:EventsCalType) {
-
+export default function EventsCalendar({events}:{events: Event[]}) {
+    const [activeStartDate, onActiveStartDate] = useState<Date>(new Date());
     const [value, onChange] = useState<Value>(new Date());
+
+    const months=['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+    const eventDays = events.map(event=>new Date(event.time).getTime());
+    const first = new Date(Math.min(...eventDays));
+    const last = new Date(Math.max(...eventDays));
+    
+    
+    // console.log(first,last);
+
 
     const tileContent = ({date, view}: TileArgs) => {
         if(view !=='month') return;
@@ -32,24 +39,45 @@ export default function EventsCalendar({events}:EventsCalType) {
             </div>
         );
     }
-    const months=['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-
+    
     const formatMonthYear = (date: Date) => {
         const year = date.getFullYear();
         const month = date.getMonth();
         return `${months[month]} ${year}`
     }
 
+    const handleActiveStartDateChanged = ({ action, activeStartDate, value, view }: OnArgs) => {
+        // console.log(action, activeStartDate,value,view)
+        if (!activeStartDate) return;
+        switch (action) {
+            case 'prev':
+            case 'prev2':
+            case 'next':
+            case 'next2':
+            case 'drillUp':
+            case 'drillDown':
+            case 'onChange':
+                if (activeStartDate <= last && activeStartDate >= new Date(first.getFullYear(), first.getMonth())) onActiveStartDate(activeStartDate);
+                break;
+            default:
+                throw new Error('bad action');
+
+        }
+    }
+
     return (
-            <Calendar 
-                calendarType='gregory' 
-                defaultView='month'
-                showNeighboringMonth={false}
-                onChange={onChange} 
-                value={value} 
-                tileContent={tileContent} 
-                formatDay={(locale,date) => ''}
-                formatMonthYear={(locale,date) => formatMonthYear(date)}
-            />
+        <Calendar 
+            calendarType='gregory' 
+            defaultView='month'
+            minDetail='year'
+            activeStartDate={activeStartDate}
+            showNeighboringMonth={false}
+            onChange={onChange} 
+            value={value} 
+            tileContent={tileContent} 
+            formatDay={(locale,date) => ''}
+            formatMonthYear={(locale,date) => formatMonthYear(date)}
+            onActiveStartDateChange={handleActiveStartDateChanged}
+        />
     )
 }
