bcdf77fca9fffc85f6f8c612642c21680ed476fc
JenniferFix <codingjennifer@gmail.com>
2024-03-04 13:04:35 -0700

    start setup and use of dynamic site data

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,50 +1,59 @@
 
-import type { Metadata } from 'next';
+import type { Metadata, ResolvingMetadata } from 'next';
 // import '@/app/globals.css';
 import Footer from '@/components/Footer';
 // import dynamic from 'next/dynamic'
 import { Suspense } from 'react';
 import Header from '@/components/HeaderNew'
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
-import {ThemeProvider} from '@mui/material/styles';
+import ThemeProvider from '@mui/material/styles/ThemeProvider';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from '../theme';
-// const Header = dynamic(() => import('@/components/Header'), {ssr: false})
-
+import getSiteSettings from '@/sanity/queries/getSiteSettings';
 export const revalidate = 3600; // revalidate at most every hour
 
-export const metadata: Metadata = {
-  title: "Calgary Non-Binary and Transgender Society",
-  description: "Calgary Non-Binary and Transgender Societies official homepage",
-  metadataBase: new URL('https://calgarynbts.ca'),
-  icons: {
-    icon: [
-      {
-      url: '/favicon-32x32.png',
-      sizes: '32x32'
+type metadataProps = {
+  params: { id: string }
+  searchParams: { [key: string]: string | string[] | undefined }
+}
+export async function generateMetadata({params, searchParams}: metadataProps, parent: ResolvingMetadata): Promise<Metadata> {
+    // read rout params
+    // const id = params.id;
+    const siteSettings = await getSiteSettings();
+  
+    return {
+      title: siteSettings.title,
+      description: siteSettings.description,
+      metadataBase: new URL('https://calgarynbts.ca'),
+      icons: {
+        icon: [
+          {
+          url: '/favicon-32x32.png',
+          sizes: '32x32'
+          },
+          {
+            url: '/favicon-16x16.png',
+            sizes: '16x16'
+          }, 
+        ],
+        apple: [
+          { url: './apple-touch-icon.png' },
+        ],
       },
-      {
-        url: '/favicon-16x16.png',
-        sizes: '16x16'
-      }, 
-    ],
-    apple: [
-      { url: './apple-touch-icon.png' },
-    ],
-  },
-  manifest: 'https://calgarynbts.ca/manifest.json',
-  robots: {
-    index: false,
-    follow: false,
-    nocache: true,
-    googleBot: {
-      index: false,
-      follow: false,
-      noimageindex: true,
+      manifest: 'https://calgarynbts.ca/manifest.json',
+      robots: {
+        index: false,
+        follow: false,
+        nocache: true,
+        googleBot: {
+          index: false,
+          follow: false,
+          noimageindex: true,
+        }
+      },
     }
-  },
-};
-
+  }
+  
 export default function RootLayout({
   children,
 }: Readonly<{
--- src/sanity/queries/getSiteSettings.tsx
+++ src/sanity/queries/getSiteSettings.tsx
@@ -1,10 +1,11 @@
 import client from '@/sanity/sanityClient';
 import { groq } from "next-sanity";
 import { SiteSettingsType } from '@/sanity/types/queryTypes';
+import { cache } from 'react';
 
-export default async function getSiteSettings(): Promise<SiteSettingsType[]> {
+async function _getSiteSettings(): Promise<SiteSettingsType> {
     return client.fetch(
-        groq`*[_type=="sitesettings"]{
+        groq`*[_type=="sitesettings"][0]{
             title,
             description,
             domainname,
@@ -14,3 +15,8 @@ export default async function getSiteSettings(): Promise<SiteSettingsType[]> {
     )
 }
 
+const getSiteSettings = cache(async () => {
+    return  await _getSiteSettings();
+})
+
+export default getSiteSettings;
