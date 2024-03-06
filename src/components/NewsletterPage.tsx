26992d768c0a771d201189d281f840db86a748bd
JenniferFix <codingjennifer@gmail.com>
2024-03-05 21:41:15 -0700

    Newsletter page

--- src/components/NewsletterPage.tsx
+++ src/components/NewsletterPage.tsx
@@ -1,10 +1,31 @@
-const NewsletterPage = ({slug}: {slug: string | undefined}) => {
+import { getNewsletter, getLatestNewsletter, getAllNewsletterHeadings } from "@/sanity/queries";
+import { notFound } from "next/navigation";
+
+import NewsletterList from "./NewsletterList";
+import Newsletter from "./Newsletter";
+
+import Box from '@mui/material/Box';
+
+type Props = {
+    slug?: string;
+}
+
+export const revalidate = 3600;
+const NewsletterPage = async ({slug}: Props) => {
+    const newsletterData = slug ? getNewsletter({slug}) : getLatestNewsletter();
+    const newsHeadingsData = getAllNewsletterHeadings();
+    const [newsletter, newsHeadings] = await Promise.all([newsletterData, newsHeadingsData]);
+    
     return (
-        <div>
-            <h1>Newsletter page</h1>
-            <p>slug: {slug}</p>
-        </div>
-    );
+    <Box sx={{display: 'flex'}}>
+        <NewsletterList newsletters={newsHeadings}/>
+        {!newsletter && notFound()}
+        {newsletter &&
+            <Newsletter title={newsletter.title} content={newsletter.body} />
+        }
+    </Box>
+    )
+    
 }
     
 export default NewsletterPage;
