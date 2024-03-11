61733072804404b02e79294cd2e31a9b85c257de
JenniferFix <codingjennifer@gmail.com>
2024-03-10 21:43:58 -0600

    Add cdn prefix

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,8 @@
 const __dirname = import.meta.dirname;
 import path from 'path';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
     webpack: (config, {isServer}) => {
         config.resolve.alias['@'] = path.join(__dirname, 'src');
@@ -17,7 +19,9 @@ const nextConfig = {
             },
         ],
         minimumCacheTTL: 60*60*24, // 1 day
-    }
+    },
+    assetPrefix: isProd ? 'https://calgarynbts.b-cdn.net' : undefined,
+    
 };
 //http://localhost:3000/(https://cdn.sanity.io/images/9108qgzh/production/252678266721c7565a418909d8ea259f9dc389fb-3300x5100.png)
 export default nextConfig;
