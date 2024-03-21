e8f99f28284b60017e45bfd10e7e3a289c033560
JenniferFix <codingjennifer@gmail.com>
2024-03-20 21:12:59 -0600

    start of resources page

--- src/components/ResourcesPage.tsx
+++ src/components/ResourcesPage.tsx
@@ -1,5 +1,41 @@
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+
 const ResourcesPage = () => {
-    return <div>Public resources for everyone to use</div>
+    return (
+        <Box>
+            <Container>
+                <Box paddingY={2}>
+                    <Accordion defaultExpanded>
+                        <AccordionSummary>
+                            <Typography>Emergency Contancts</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Typography>911</Typography>
+                            <Typography>988</Typography>
+                        </AccordionDetails>
+                    </Accordion>
+                    <Accordion>
+                        <AccordionSummary>Mental Health Supports</AccordionSummary>
+                        <AccordionDetails>List here</AccordionDetails>
+                    </Accordion>
+                    <Accordion>
+                        <AccordionSummary>Pride Organizations</AccordionSummary>
+                        <AccordionDetails>List here</AccordionDetails>
+                    </Accordion>
+                    <Accordion>
+                        <AccordionSummary>Social Support Groups</AccordionSummary>
+                        <AccordionDetails>List here</AccordionDetails>
+                    </Accordion>
+                </Box>
+            </Container>
+        </Box>
+    );
 }
-    
-    export default ResourcesPage;
+
+export default ResourcesPage;
