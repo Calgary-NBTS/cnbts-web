43c71b05714ba8a1b218ea34f853ded213b0ab41
JenniferFix <codingjennifer@gmail.com>
2024-05-21 19:40:39 -0600

    added image placeholder

--- src/components/calendar/CalendarTileContent.tsx
+++ src/components/calendar/CalendarTileContent.tsx
@@ -1,119 +1,131 @@
-'use client';
-import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+"use client";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
-import Image from 'next/image';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Popover from '@mui/material/Popover';
-import Tooltip, { TooltipProps, tooltipClasses } from '@mui/material/Tooltip';
-import {styled} from '@mui/material/styles';
-import useTheme from '@mui/material/styles/useTheme';
-import FormattedText from '../primative/FormattedText';
-import { Event } from '@/sanity/types/queryTypes';
+import Image from "next/image";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Popover from "@mui/material/Popover";
+import Tooltip, { TooltipProps, tooltipClasses } from "@mui/material/Tooltip";
+import { styled } from "@mui/material/styles";
+import useTheme from "@mui/material/styles/useTheme";
+import FormattedText from "../primative/FormattedText";
+import { Event } from "@/sanity/types/queryTypes";
+import RainbowHeart from "@/../public/images/RainbowHeart.svg";
 
-import EventPoster from '@/components/common/EventPoster';
+import EventPoster from "@/components/common/EventPoster";
 
 type ValuePiece = Date | null;
 type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 type Props = {
-    date: Date;
-    event: Event | undefined;
-}
+  date: Date;
+  event: Event | undefined;
+};
 
-const CalendarTileContent = ({date, event}: Props) => {
-    const theme = useTheme();
-    const day = date.getDate();
+const CalendarTileContent = ({ date, event }: Props) => {
+  const theme = useTheme();
+  const day = date.getDate();
 
-    const TooltipContent = ({event}: {event: Event}) => {
-        if (!event) return;
-        return (
-            <Box>
-                <Typography className="text-xl">{event.name}</Typography>
-                <p>From: {new Date(event.time).toLocaleTimeString('en-CA')} to {new Date(event.timeend).toLocaleTimeString('en-CA')}</p>
-                <p>Location: {event.location || 'Unknown'}</p>
-                <div><FormattedText value={event.content} /></div>
-            </Box>
-        )
-    }
-
-    const HtmlTooltip = styled(({ className, ...props }: TooltipProps) => (
-        <Tooltip enterTouchDelay={200} {...props} classes={{ popper: className }} />
-      ))(({ theme }) => ({
-        [`& .${tooltipClasses.tooltip}`]: {
-          backgroundColor: theme.vars.palette.background.paper,
-          color: theme.vars.palette.primary.contrastText,
-          maxWidth: 220,
-          fontSize: theme.typography.pxToRem(12),
-          border: '1px solid #dadde9',
-        },
-      }));
-
-    const StyledTile = styled('div')(({ theme }) => ({
-        position:'relative',
-        height:'100%',
-        display: 'flex',
-        flexDirection: 'column',
-        alignContent: 'flex-start',
-        borderRadius: '0.75rem',
-        padding: '0 0.2rem',
-        color: theme.vars.palette.primary.contrastText,
-        backgroundColor: theme.vars.palette.background.paper,
-        boxShadow: '2px 2px 5px 2px rgba(0,0,0,0.2)',
-        [theme.getColorSchemeSelector('dark')]: {
-            boxShadow: '1px 1px 5px 2px rgba(0,0,0,0.3)'
-        }
-    }))
-
+  const TooltipContent = ({ event }: { event: Event }) => {
+    if (!event) return;
     return (
-        <StyledTile>
-            <Box
+      <Box>
+        <Typography className="text-xl">{event.name}</Typography>
+        <p>
+          From: {new Date(event.time).toLocaleTimeString("en-CA")} to{" "}
+          {new Date(event.timeend).toLocaleTimeString("en-CA")}
+        </p>
+        <p>Location: {event.location || "Unknown"}</p>
+        <div>
+          <FormattedText value={event.content} />
+        </div>
+      </Box>
+    );
+  };
+
+  const HtmlTooltip = styled(({ className, ...props }: TooltipProps) => (
+    <Tooltip enterTouchDelay={200} {...props} classes={{ popper: className }} />
+  ))(({ theme }) => ({
+    [`& .${tooltipClasses.tooltip}`]: {
+      backgroundColor: theme.vars.palette.background.paper,
+      color: theme.vars.palette.primary.contrastText,
+      maxWidth: 220,
+      fontSize: theme.typography.pxToRem(12),
+      border: "1px solid #dadde9",
+    },
+  }));
+
+  const StyledTile = styled("div")(({ theme }) => ({
+    position: "relative",
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+    alignContent: "flex-start",
+    borderRadius: "0.75rem",
+    padding: "0 0.2rem",
+    color: theme.vars.palette.primary.contrastText,
+    backgroundColor: theme.vars.palette.background.paper,
+    boxShadow: "2px 2px 5px 2px rgba(0,0,0,0.2)",
+    [theme.getColorSchemeSelector("dark")]: {
+      boxShadow: "1px 1px 5px 2px rgba(0,0,0,0.3)",
+    },
+  }));
+
+  return (
+    <StyledTile>
+      <Box
+        sx={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+        }}
+      >
+        <Typography variant="body1" component="h4">
+          {day}
+        </Typography>
+      </Box>
+      <Box
+        sx={{
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          paddingTop: { xxs: "1rem", xs: "1rem", sm: "1rem", md: "0" },
+        }}
+      >
+        {event && (
+          <>
+            <HtmlTooltip title={<TooltipContent event={event} />}>
+              <Box
                 sx={{
-                    position: 'absolute',
-                    top:0,
-                    left:0,
-                    right:0
+                  maxHeight: "60px",
+                  maxWidth: "60px",
+                  position: "relative",
                 }}
-            >
-                    <Typography variant='body1' component='h4'>
-                        {day}
-                    </Typography>
-                </Box>
-                <Box 
-                    sx={{
-                        height: '100%', 
-                        display: 'flex', 
-                        alignItems: 'center', 
-                        justifyContent: 'center',
-                        paddingTop: {xxs: '1rem', xs: '1rem', sm: '1rem', md: '0'}
-                    }}
-                >
-                    {event &&
-                        <>
-                            <HtmlTooltip title={<TooltipContent event={event} />}>
-                                <Box 
-                                    sx={{maxHeight:'60px', maxWidth: '60px', position: 'relative'}}
-                                >
-                                    <Image 
-                                        src={event.image} 
-                                        width={event.imageWidth}
-                                        height={event.imageHeight}
-                                        alt={event.name}
-                                        sizes='(max-width: 1200px) 15vw, 10vw'
-                                        style={{
-                                            width: '100%',
-                                            height: 'auto',
-                                        }}
-                                    />
-                                </Box>
-                            </HtmlTooltip>
-                            <Popover open={false}><EventPoster event={event} /></Popover>
-                        </>
-                    }
-                </Box>
-            </StyledTile>
-    )
-}
+              >
+                <Image
+                  src={event.image ? event.image : RainbowHeart}
+                  width={event.imageWidth}
+                  height={event.imageHeight}
+                  alt={event.name ? event.name : ""}
+                  sizes="(max-width: 1200px) 15vw, 10vw"
+                  style={{
+                    width: "100%",
+                    height: "auto",
+                  }}
+                />
+              </Box>
+            </HtmlTooltip>
+            <Popover open={false}>
+              <EventPoster event={event} />
+            </Popover>
+          </>
+        )}
+      </Box>
+    </StyledTile>
+  );
+};
 
 export default CalendarTileContent;
