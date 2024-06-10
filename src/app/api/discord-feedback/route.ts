f9be38b802f7b860bc276b2fab55efc77a45dcb8
JenniferFix <codingjennifer@gmail.com>
2024-06-09 21:03:51 -0600

    more testing

--- src/app/api/discord-feedback/route.ts
+++ src/app/api/discord-feedback/route.ts
@@ -1,6 +1,8 @@
+import { NextRequest } from 'next/server'
+
 export const dynamic = 'force-dynamic' // defaults to auto
 
-export async function GET(request: Request) {
+export async function GET(request: Request, response: Response) {
   console.log(request)
   return new Response('Hello World!', {
     status: 200,
@@ -13,8 +15,9 @@ export async function GET(request: Request) {
 export async function POST(request: Request) {
 
   console.log('request', request)
+  console.log('body', await request.json())
 
-  return new Response('Hello World!', {
+  return new Response('Success', {
     status: 200,
     headers: {
       'content-type': 'text/plain',
