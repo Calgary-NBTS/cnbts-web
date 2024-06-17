3e50bf0b599a2395c7c75d3e53c3219c07c5dcd1
JenniferFix <codingjennifer@gmail.com>
2024-06-17 12:32:13 -0600

    add toMarkdown to module

--- decs.d.ts
+++ decs.d.ts
@@ -1,1 +1,7 @@
-declare module "@sanity/block-content-to-markdown" 
+import { PortableTextBlock } from 'next-sanity';
+
+declare module "@sanity/block-content-to-markdown" {
+  export function toMarkdown(any: PortableTextBlock[]): string;
+
+}
+f
